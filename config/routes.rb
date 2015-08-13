e8deb7e98f90e24fe8d2d622623578f2fbb6c813
Charlie Chrisman <charlie.chrisman@gmail.com>
2015-08-13 01:18:39 -0700

    troubleshooting active calls view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get '/calls/active', to: 'calls#active', as: 'active_calls'
   resources :calls
   root 'calls#new'
-  get '/calls/active', to: 'calls#active', as: 'active_calls'
   post '/calls/:id', to: 'calls#update'
   post '/callback/hangup/:id', to: 'calls#hangup_callback', as: 'hangup_callback'
 end
